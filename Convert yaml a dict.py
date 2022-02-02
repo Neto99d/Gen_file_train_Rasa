419a5fb10526c6dcb117eea4ec977eed4ef8fe9e
Neto99d <ernesto.duvalon@gmail.com>
2022-02-02 17:35:22 -0500

    Update Convert yaml a dict.py

--- Convert yaml a dict.py
+++ Convert yaml a dict.py
@@ -3,8 +3,7 @@
 import yaml
 from yaml.loader import SafeLoader
 
-  # such output is not valid YAML!
-# Open the file and load the file
+ 
 print("Entre la direccion del archivo .yaml:")
 with open(input(), 'r') as f:
     data = yaml.load(f, Loader=SafeLoader)
