f4c4f363addf0a7b95f19a794894a5b8683ce00d
Neto99d <ernesto.duvalon@gmail.com>
2022-08-22 20:03:46 -0400

    Update entrenarAsistenteES.py

--- src/entrenarAsistenteES.py
+++ src/entrenarAsistenteES.py
@@ -19,8 +19,6 @@ def mover():
                     os.path.join(dir + "\data", "stories.yml"))
         shutil.move(dirname + os.path.sep + "Archivos_generados" + os.path.sep + "rules.yml",
                     os.path.join(dir + "\data", "rules.yml"))
-        # shutil.move(dirname + os.path.sep + "Archivos_generados" + os.path.sep + "config.yml",
-                    # os.path.join(dir, "config.yml"))
         print("Archivos movidos a la carpeta del Asistente para ser entrenado" + '\n')
         return True
 
